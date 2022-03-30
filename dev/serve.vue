1eb7208b42cf0a2ab5b6935a4347058920ca6669
bojun1995 <bojun1995@foxmail.com>
2022-03-30 23:25:10 +0800

    【修改】使用vue-sfc-rollup

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue'
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ElementVDlcSample } from '@/entry.esm';
+
+export default Vue.extend({
+	name: 'ServeDev',
+	// components: {
+	//  ElementVDlcSample,
+	// }
+})
+</script>
+
+<template>
+	<div id="app">
+		<element-v-dlc-sample />
+	</div>
+</template>
--- packages/VirtualizedSelect/index.js
+++
@@ -1,5 +0,0 @@
-import VirtualizedSelect from './src/index.vue'
-VirtualizedSelect.install = function (Vue) {
-	Vue.component(VirtualizedSelect.name, VirtualizedSelect)
-}
-export default VirtualizedSelect
