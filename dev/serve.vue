68c8a2ed5ebd7cf619fe543e7fe31d0715fe4e22
bojun1995 <bojun1995@foxmail.com>
2022-03-31 23:37:41 +0800

    【修改】虚拟滚动demo

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,13 +1,31 @@
+<template>
+	<div id="app">
+		<el-v-select :data="cp_options"></el-v-select>
+	</div>
+</template>
+
 <script>
 import Vue from 'vue'
 
 export default Vue.extend({
 	name: 'ServeDev',
+	data() {
+		return {
+
+    }
+	},
+  computed: {
+    cp_options() {
+      let ret = []
+      for(let i = 0; i < 10000; i++) {
+        ret.push({
+          label: `label-${i}`,
+          value: i,
+          id: i,
+        })
+      }
+      return ret
+    }
+  }
 })
 </script>
-
-<template>
-	<div id="app">
-		<el-v-select></el-v-select>
-	</div>
-</template>
--- src/lib-components/el-v-select/index.vue
+++ src/lib-components/el-v-select/index.vue
@@ -1,27 +1,78 @@
 <template>
-  <div class="el-v-select">ElVSelect</div>
+	<div>
+		<div>
+			<input type="text" class="input" />
+		</div>
+		<div :style="`height: ${dropDownHeight}px;`" @scroll="onScroll" class="drop-down-box">
+			<div :style="`height: ${scrollHeight}px`">
+				<div :style="`transform: translateY(${scrollOffsetY}px); `">
+					<div v-for="item in showItemList" :key="item.id" :style="`height: ${itemHeight}px; line-height: ${itemHeight}px`" class="drop-down-item">
+						<span>{{ item.label }}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'ElVSelect',
-  data() {
-    return {}
-  },
-  computed: {},
-  watch: {},
-  created() { },
-  mounted() { },
-  methods: {},
+	name: 'ElVSelect',
+	props: {
+		data: Array,
+		dropDownHeight: {
+			type: Number,
+			default: 200,
+		},
+		itemHeight: {
+			type: Number,
+			default: 50,
+		},
+	},
+	data() {
+		return {
+			lastScrollTime: '', // 上次执行滚动事件的时刻
+			scrollHeight: '', // 列表总高度
+			showItemList: [], // 展示列表
+			scrollOffsetY: '', // 动态偏移量
+			showItemNum: '', // 显示的个数
+		}
+	},
+	mounted() {
+		// 计算总高度
+		this.scrollHeight = this.itemHeight * this.data.length
+		// 计算可视区域能展示的个数 这里可以随机多加几个让滚动有个临界区间，避免向下滑动时元素直接替换
+		this.showItemNum = this.dropDownHeight / this.itemHeight + 4
+		this.lastScrollTime = new Date().getTime()
+		// 默认展示
+		this.showItemList = this.data.slice(0, this.showItemNum)
+	},
+	methods: {
+		onScroll(e) {
+			let curTime = new Date().getTime()
+      console.log('do scroll')
+			this.scrollOffsetY = e.target.scrollTop - (e.target.scrollTop % this.itemHeight)
+
+			let startIdx = Math.floor(e.target.scrollTop / this.itemHeight)
+			let endIdx = Math.floor(e.target.scrollTop / this.itemHeight) + this.showItemNum
+			this.showItemList = this.data.slice(startIdx, endIdx)
+
+			this.lastScrollTime = new Date().getTime()
+		},
+	},
 }
 </script>
 
 <style scoped>
-.el-v-select {
-  height: 35px;
-  width: 100px;
-  line-height: 35px;
-  text-align: center;
-  border: 1px solid #000000;
+.input {
+	width: 290px;
+	margin-bottom: 10px;
+}
+.drop-down-box {
+	overflow-y: scroll;
+	width: 300px;
+	border: 1px solid #000000;
+}
+.drop-down-item {
 }
 </style>
